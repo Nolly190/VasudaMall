500789b2b2eedb5dbc97a891b9f22abe1f576c2e
CHIZOBA NNAMANI <SN029470@firstbanknigeria.com>
2020-06-30 15:01:58 +0100

    (ft-add-view): Fix jquery issue on dashboard view [FIXES}

--- VasudaMall/VasudaMall/App_Start/BundleConfig.cs
+++ VasudaMall/VasudaMall/App_Start/BundleConfig.cs
@@ -40,8 +40,8 @@ namespace VasudaMall
                 ));
 
             bundles.Add(new ScriptBundle("~/vasuda-template/script").Include(
-                    "~/vasuda-template/js/jquery-3.3.1.min.js",
                     "~/vasuda-template/js/jquery-2.0.3.min.js",
+                    "~/vasuda-template/js/jquery-3.3.1.min.js",
                     "~/vasuda-template/js/bootstrap.min.js",
                     "~/vasuda-template/js/bigSlide.js",
                     "~/vasuda-template/js/index.js",
@@ -55,10 +55,10 @@ namespace VasudaMall
             bundles.Add(new ScriptBundle("~/admin-lte/script").Include(
                 // "~/Scripts/jquery-1.10.2.min.js",
                 //"~/Scripts/jquery-3.4.1.min.js",
-                "~/admin-lte/dist/js/adminlte.js",
-                "~/admin-lte/dist/js/pages/dashboard.js",
                 "~/admin-lte/plugins/jquery/jquery.min.js",
                 "~/admin-lte/plugins/jquery-ui/jquery-ui.min.js",
+                "~/admin-lte/dist/js/adminlte.js",
+                "~/admin-lte/dist/js/pages/dashboard.js",
                 "~/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js",
                 "~/admin-lte/plugins/jqvmap/jquery.vmap.min.js",
                 "~/admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js",
@@ -70,9 +70,10 @@ namespace VasudaMall
                 "~/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js",
                 "~/admin-lte/plugins/datatables/jquery.dataTables.js",
                 "~/admin-lte/plugins/datatables-bs4/js/dataTables.bootstrap4.js",
-                "~/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js",
-                "~/Scripts/jquery.validate.min.js",
-                "~/Scripts/jquery.validate.unobtrusive.min.js"));
+                "~/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"
+                //"~/Scripts/jquery.validate.min.js",
+                //"~/Scripts/jquery.validate.unobtrusive.min.js"
+                ));
 
             bundles.Add(new StyleBundle("~/admin-lte/css").Include(
                 "~/admin-lte/dist/css/adminlte.min.css",
