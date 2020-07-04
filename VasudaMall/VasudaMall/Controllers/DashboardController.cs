19e52ae89baeec4e1baa9cb5975984ffcb76e722
CHIZOBA NNAMANI <SN029470@firstbanknigeria.com>
2020-07-04 21:42:35 +0100

    (ft-add-view-html): Add html for the views of the dashboard pages. [Start]

--- VasudaMall/VasudaMall/Controllers/DashboardController.cs
+++ VasudaMall/VasudaMall/Controllers/DashboardController.cs
@@ -6,11 +6,31 @@ using System.Web.Mvc;
 
 namespace VasudaMall.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class DashboardController : Controller
     {
         // GET: Dashboard
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public new ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult Notification()
+        {
+            return View();
+        }
+        
+        public ActionResult Transaction()
+        {
+            return View();
+        }
+        
+        public ActionResult Wallet()
         {
             return View();
         }
