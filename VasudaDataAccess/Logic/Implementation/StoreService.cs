cc27e20150d8149fa131e993d45fd4099dfc42e0
udenwani nonso <nolly190@gmail.com>
2020-07-07 08:51:34 +0100

    Home Page Done

---
+++ VasudaDataAccess/Logic/Implementation/StoreService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using VasudaDataAccess.Data_Access.Implentations;
+using VasudaDataAccess.DTOs;
+using VasudaDataAccess.Model;
+
+namespace VasudaDataAccess.Logic.Implementation
+{
+    public class StoreService : IStoreService
+    {
+        private UnitOfWork _unitOfWork;
+        private Logger logger;
+
+        public StoreService()
+        {
+            logger = LogManager.GetCurrentClassLogger();
+            _unitOfWork = new UnitOfWork(new VasudaModel());
+        }
+        public Response<HomeProductViewModel> GetHomePage()
+        {
+            var result = new Response<HomeProductViewModel>();
+            result.Status = false;
+            result.Message = "Could not retrieve info";
+            var model = new HomeProductViewModel();
+            model.TopStory = new List<ProductTable>();
+            model.Popular = new List<ProductTable>();
+            result.SetResult(model);
+            try
+            {
+                
+                model.Popular = _unitOfWork.ProductTable.GetAllPopular(null);
+                model.TopStory = _unitOfWork.ProductTable.GetAllTrending(null);
+                result.Status = true;
+                result.SetResult(model);
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ VasudaMall/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VasudaDataAccess.Logic;
+
+namespace VasudaMall.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreService _storeService;
+
+        public HomeController(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
+        public ActionResult Index()
+        {
+            return View(_storeService.GetHomePage().Result());
+        }
+      
+
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Terms()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        public ActionResult Teams()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
